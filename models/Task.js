1c5dde2c472b92965c45d8123026c77ee2dd3ef1
Kiran Bhat <kiranbhat2005@gmail.com>
2018-02-26 15:02:24 +0530

    moved 'task' files to project from loopback-models-taks module.

---
+++ models/Task.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function (Task) {
+  var methodNames = [
+    "replaceOrCreate", "patchOrCreate", 
+    "destroyById",
+    "count", "exists", "replaceById", "prototype.patchAttributes",
+    "replaceById", "upsertWithWhere"
+  ];
+
+  methodNames.forEach(function (name) {
+    Task.disableRemoteMethodByName(name);
+  });
+
+  Task.prototype.complete = function (data, options, cb) {
+    console.log('DATA', data , 'options', options);
+    return this.updateAttributes({ status: 2 }).then((result) => {
+      Task.emit('complete', this);
+      return;
+    });
+  };
+
+  var completeOptions = {
+    description: 'Patch attributes for a model instance and persist it into ' +
+      'the data source.',
+    accessType: 'WRITE',
+    accepts: [
+      {
+        arg: 'data', type: 'object', model: 'any',
+        http: { source: 'body' },
+        description: 'An object of formValue property name/value pairs',
+      },
+      { arg: 'options', type: 'object', http: 'optionsFromRequest' },
+    ],
+    returns: { arg: 'data', type: 'any', root: true },
+    http: [{ verb: 'put', path: '/complete' }],
+  };
+
+  Task.remoteMethod('prototype.complete', completeOptions);
+};
